9273ef9efe51a3b90961cce6cfe002fffe82ab5f
PowChorba <101152834+PowChorba@users.noreply.github.com>
2022-09-11 09:46:00 -0300

    Sin console log

--- client/src/context/authContext.js
+++ client/src/context/authContext.js
@@ -54,7 +54,6 @@ export function AuthProvider({ children }) {
 
   useEffect(() => {
     onAuthStateChanged(auth, (currentUser) => {
-      console.log({ currentUser });
       setUser(currentUser);
       setLoading(false);
     });
